80aa0d98f3fabbc6fa8348a63fa2188637ea5364
Aliza Berman <alizaberman46@gmail.com>
2020-05-12 21:02:15 -0400

    Add project files.

---
+++ Homework5.06/ClientApp/src/PeopleTable.js
@@ -0,0 +1,72 @@
+﻿import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import PersonForm from './PersonForm';
+import PersonRow from './PersonRow';
+
+class PeopleTable extends React.Component {
+
+    state = {
+        people: [],
+        person: {
+            firstName: '',
+            lastName: '',
+            age: ''
+        }
+    }
+    handleFirstName = e => {
+        const personCopy = { ...this.state.person };
+        personCopy.firstName = e.target.value;
+        this.setState({ person: personCopy });
+    }
+    handleLastName = e => {
+        const personCopy = { ...this.state.person };
+        personCopy.lastName = e.target.value;
+        this.setState({ person: personCopy });
+    }
+    handleAge = e => {
+        const personCopy = { ...this.state.person };
+        personCopy.age = e.target.value;
+        this.setState({ person: personCopy });
+    }
+    onAddClick = () => {
+        const peopleCopy = [...this.state.people];
+        const person = {
+            firstName: this.state.person.firstName,
+            lastName: this.state.person.lastName,
+            age: this.state.person.age
+        };
+        peopleCopy.push(person);
+        this.setState({ people: peopleCopy, person: { firstName: '', lastName: '', age: '' } });
+    };
+    onClearClick = () => {
+        this.setState({ people: [] });
+    };
+    render() {
+        return (
+            <div className="container" style={{ marginTop: 60 }}>
+                <PersonForm
+                    handleFirstName={this.handleFirstName}
+                    handleLastName={this.handleLastName}
+                    handleAge={this.handleAge}
+                    firstNameVal={this.state.person.firstName}
+                    lastNameVal={this.state.person.lastName}
+                    ageVal={this.state.person.age}
+                    onAddClick={this.onAddClick}
+                    onClearClick={this.onClearClick}
+                />
+                <table className="table table-hover table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Age</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.people.map((p, i) => <PersonRow person={p} key={i} />)}
+                    </tbody>
+                </table>
+            </div>);
+    }
+}
+export default PeopleTable
---
+++ Homework5.06/ClientApp/src/PersonForm.js
@@ -0,0 +1,49 @@
+﻿import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class PersonForm extends React.Component {
+    render() {
+        return (
+            <div className="row" style={{ marginBottom: 10 }}> 
+                <div className="col-md-2">
+                    <input
+                        onChange={this.props.handleFirstName}
+                        value={this.props.firstNameVal}
+                        className="form-control"
+                        placeholder="First Name"
+                        type="text" />
+                </div>
+                <div className="col-md-2">
+                    <input
+                        onChange={this.props.handleLastName}
+                        value={this.props.lastNameVal}
+                        className="form-control"
+                        placeholder="Last Name"
+                        type="text" />
+                </div>
+                <div className="col-md-2">
+                    <input
+                        onChange={this.props.handleAge}
+                        value={this.props.ageVal}
+                        className="form-control"
+                        placeholder="Age"
+                        type="text" />
+                </div>
+               <div className="col-md-2">
+                    <button
+                        className="btn btn-primary btn-block"
+                        onClick={this.props.onAddClick}
+                    >Add</button>
+                </div>
+                <div className="col-md-2">
+               
+                    <button
+                        className="btn btn-success btn-block"
+                        onClick={this.props.onClearClick}
+                    >Clear</button>
+                </div>
+            </div>
+        );
+    }
+}
+export default PersonForm;
